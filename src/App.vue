ab68f3c5601497057e880ddef3eea801213c85a8
Filip Skoczen <philipskoczen@gmail.com>
2022-01-19 17:16:36 +0100

    Create form

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
+<script setup lang="ts">
+import { $ref } from 'vue/macros';
+import arrow from '~/assets/images/arrow.svg';
+
+let email = $ref('');
+let error = $ref(0);
+
+const submit = () => {
+  if (/([\w\d.]+)@\w+\.\w+/.test(email)) {
+    error = -1;
+  } else {
+    error = 1;
+  }
+}
+</script>
+
 <template>
-  <div class='flex items-center justify-center h-screen text-3xl gap-2'>
-    Hello
-    <span class="text-[#3EAF7C]">Vue3</span>
-    +
-    <span class="text-[#9B4EF2]">Vite</span>
-    +
-    <span class="text-[#41A0E5]">WindiCSS</span>
+  <div class="flex items-center justify-center h-screen text-[#32475b]">
+    <form @submit.prevent="submit">
+      <h1 class="text-3xl font-bold mb-8">
+        Sign up from the <br/> latest updates
+      </h1>
+      <p class="uppercase font-semibold text-[#3fb27f] text-sm">email address</p>
+      <div class="border-1 hover:border-[#3fb27f]">
+        <input v-model="email" class="px-2">
+        <button class="bg-[#3fb27f] h-10 px-3">
+          <img :src="arrow" alt="">
+        </button>
+      </div>
+      <div v-if="error === 1" class="border-1 border-[#B71540] text-[#B71540] bg-[#FDEFF3] p-4 mt-4">
+        <h1 class="font-semibold">Please use a valid email</h1>
+        <p class="text-sm">Like: person@inbox.com</p>
+      </div>
+      <div v-else-if="error === -1" class="border-1 border-[#258A85] text-[#258A85] bg-[#F5FFFE] p-4 mt-4">
+        <h1 class="font-semibold">Yay! Thank you</h1>
+        <p class="text-sm">We've send a confirmation <br /> link to your inbox</p>
+      </div>
+    </form>
   </div>
 </template>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import "virtual:windi.css";
+import "~/assets/style/global.css";
 import 'virtual:windi-devtools'
 
 createApp(App).mount('#root')
